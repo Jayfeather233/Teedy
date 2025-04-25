649c7017862a04b7355c0a523e15250980765a24
jayfeather233 <jayfeather233@gmail.com>
2025-04-25 14:07:29 +0800

    feat: better UI and auto pull message & lab10

--- rerun.sh
+++ rerun.sh
@@ -9,3 +9,7 @@ mvn clean -Dmaven.test.failure.ignore=true -Dpmd.failOnViolation=false install s
 mvn clean -DskipTests -Dmaven.test.failure.ignore=true -Dpmd.failOnViolation=false install
 cd docs-web
 mvn jetty:run
+
+
+mvn clean install -Pprod -DskipTests -Dmaven.test.failure.ignore=true -Dpmd.failOnViolation=false
+mvn jetty:run-war -Pprod -DskipTests
